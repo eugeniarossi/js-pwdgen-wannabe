5dbf165610feefff4373a46f9bdcc2f9901129ef
eugeniarossi <eugenia.rossi@outlook.com>
2023-02-20 16:37:07 +0100

    stile pagina

--- JS/script.js
+++ JS/script.js
@@ -26,5 +26,5 @@ console.log(generatedPassword);
 
 // mostro la password sulla pagina
 
-document.querySelector('span').innerHTML = generatedPassword;
+document.getElementById('user_password').innerHTML = generatedPassword;
 
